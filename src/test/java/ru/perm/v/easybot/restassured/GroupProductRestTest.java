58b24b152b287284128ca706b316d4b4ebb4714c
Черепахин Василий <vasi.che@gmail.com>
2023-06-09 10:36:03 +0500

    delete epic feature story

--- src/test/java/ru/perm/v/easybot/restassured/GroupProductRestTest.java
+++ src/test/java/ru/perm/v/easybot/restassured/GroupProductRestTest.java
@@ -13,24 +13,14 @@ public class GroupProductRestTest {
 
     private final static String GROUP_PRODUCT_PATH = HOST + "/group_product";
 
-    @Epic("REST API Country")
-    @Feature("Verify Operations getAll(), getById(id) on Country")
-    @Story("Story Country GET ID Request")
-
     @DisplayName("Country GET ID Request id=1 is status=200")
-    @Severity(SeverityLevel.NORMAL)
     @Description(value = "Verify the HTTP answer of country id=1 is status=200")
     @Test
     public void getGrouProductId_1_and_StatusCode_200() {
         given().when().get(GROUP_PRODUCT_PATH + "/1").then().statusCode(HttpStatus.SC_OK);
     }
 
-    @Epic("REST API Country")
-    @Feature("Verify Operations getAll(), getById(id) on Country")
-    @Story("Story Country GET ID Request")
-
     @DisplayName("Country GET all country is status=200")
-    @Severity(SeverityLevel.NORMAL)
     @Description(value = "Verify the HTTP answer GET all products is status=200")
     @Test
     public void getGrouProductAll_and_StatusCode_200() {
