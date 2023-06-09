92d0c4d6798cb66a5a6faea584849dc9f2c566f4
Черепахин Василий <vasi.che@gmail.com>
2023-06-09 11:51:32 +0500

    clear not used annotations

--- src/test/java/ru/perm/v/easybot/restassured/EchoRestTest.java
+++ src/test/java/ru/perm/v/easybot/restassured/EchoRestTest.java
@@ -8,17 +8,12 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static ru.perm.v.easybot.restassured.VARS.HOST;
 
-@DisplayName("Tests GET EchoRest")
+@DisplayName("Tests EchoRestTest")
 public class EchoRestTest {
 
     private final static String GROUP_PRODUCT_PATH = HOST + "/group_product";
 
-    @Epic("REST API Echo")
-    @Feature("Verify Operations echo")
-
     @DisplayName("Get echo is status=200")
-    @Severity(SeverityLevel.NORMAL)
-    @Description(value = "Verify the HTTP answer is status=200")
     @Test
     public void getEcho_and_StatusCode_200() {
         given().when().get(GROUP_PRODUCT_PATH + "/echo").then().statusCode(HttpStatus.SC_OK);
--- src/test/java/ru/perm/v/easybot/restassured/GroupProductRestTest.java
+++ src/test/java/ru/perm/v/easybot/restassured/GroupProductRestTest.java
@@ -10,20 +10,18 @@ import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static ru.perm.v.easybot.restassured.VARS.HOST;
 
-@DisplayName("Tests GET GroupProductRest")
+@DisplayName("Tests GroupProductRest")
 public class GroupProductRestTest {
 
     private final static String GROUP_PRODUCT_PATH = HOST + "/group_product";
 
     @DisplayName("Country GET ID Request id=1 is status=200 is OK")
-    @Description(value = "Verify the HTTP OK answer of country id=1 is status=200")
     @Test
     public void getByGroupProductIdStatusCode_200() {
         given().when().get(GROUP_PRODUCT_PATH + "/1").then().statusCode(HttpStatus.SC_OK);
     }
 
     @DisplayName("Country GET ID Request id=1")
-    @Description(value = "Verify the HTTP OK answer of country id=1")
     @Test
     public void getByGroupProductId_1() {
         GroupProductDTO example = new GroupProductDTO(1L, "IT products",-1L, false);
