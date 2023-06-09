f59f6cdc7e1f472c0431332c78a1606a3472a1f9
Черепахин Василий <vasi.che@gmail.com>
2023-06-09 18:26:41 +0500

    add ProductRestTest.getProductsByGroupId_3

--- src/test/java/ru/perm/v/easybot/restassured/ProductRestTest.java
+++ src/test/java/ru/perm/v/easybot/restassured/ProductRestTest.java
@@ -3,7 +3,6 @@ package ru.perm.v.easybot.restassured;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import ru.perm.v.easybot.restassured.dto.GroupProductDTO;
 import ru.perm.v.easybot.restassured.dto.ProductDTO;
 
 import static io.restassured.RestAssured.given;
@@ -17,16 +16,28 @@ public class ProductRestTest {
 
     @DisplayName("GET Product by ID Request id=31 is status=200 is OK")
     @Test
-    public void getByProductById_31_StatusCode_200() {
+    public void getProductById_31_StatusCode_200() {
         given().when().get(PRODUCT_PATH + "/31").then().statusCode(HttpStatus.SC_OK);
     }
 
     @DisplayName("Check DTO by GET Product with ID=31")
     @Test
-    public void getByProductId_31_checkDTO() {
+    public void getProductById_31_checkDTO() {
         ProductDTO example = new ProductDTO(31L, "Desktop1", 3L);
         ProductDTO dto = given().when().get(PRODUCT_PATH + "/31").andReturn().as(ProductDTO.class);
         assertEquals(example, dto);
     }
 
+    @DisplayName("GET by GROUP_ID=3")
+    @Test
+    public void getProductsByGroupId_3() {
+        Long GROUP_ID = 3L;
+
+        ProductDTO[] dtos = given().when().get(PRODUCT_PATH + "/groupId/" + GROUP_ID).as(ProductDTO[].class);
+
+        assertEquals(3, dtos.length);
+        assertEquals(new ProductDTO(31L, "Desktop1", GROUP_ID), dtos[0]);
+        assertEquals(new ProductDTO(32L, "Desktop2", GROUP_ID), dtos[1]);
+        assertEquals(new ProductDTO(33L, "Desktop3", GROUP_ID), dtos[2]);
+    }
 }
